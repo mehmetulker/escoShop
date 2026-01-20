c43f8196c6c29c0538c0c63d156ced6c5a405fa9
mehmet ülker <mehmet-ulker@hotmail.com>
2026-01-20 22:54:34 +0300

    Add new products section with images, styles, and slider functionality

--- js/script.js
+++ js/script.js
@@ -7,3 +7,46 @@ mobileMenuBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
   mobileMenuBtn.classList.toggle("active");
 });
+
+const slider = document.querySelector(".new-products-products");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+const scrollAmount = 336; // 312 + gap
+
+next.addEventListener("click", () => {
+  slider.scrollLeft += scrollAmount;
+});
+
+prev.addEventListener("click", () => {
+  slider.scrollLeft -= scrollAmount;
+});
+
+const sliders = document.querySelector(".new-products-products");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+sliders.addEventListener("mousedown", (e) => {
+  isDown = true;
+  sliders.classList.add("active");
+  startX = e.pageX - sliders.offsetLeft;
+  scrollLeft = sliders.scrollLeft;
+});
+
+sliders.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+sliders.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+sliders.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - sliders.offsetLeft;
+  const walk = (x - startX) * 1.2; // hız
+  sliders.scrollLeft = scrollLeft - walk;
+});
